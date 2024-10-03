7b3ee11ba66b5e17d653ae74a274d864f236681f
Nacho42O <ignaciov1405@gmail.com>
2024-10-03 18:43:16 -0300

    ultima actualizacion

--- src/app/personajes/page.js
+++ src/app/personajes/page.js
@@ -6,9 +6,17 @@ export default function Personaje() {
   const searchParams = useSearchParams();
   const nombre = searchParams.get('nombre'); // Obtener el nombre del personaje de la URL
 
+  // Cambiar el nombre a minúsculas para que coincida con el nombre del archivo
+  const imageName = `${nombre.toLowerCase()}.png`;
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
       <h1 className="text-4xl font-bold text-white">{`Hola ${nombre}`}</h1>
+      <img
+        src={`/${imageName}`} // Ruta de la imagen
+        alt={nombre}
+        className="mt-4 w-1/2 h-auto" // Tamaño de la imagen (ajustar como desees)
+      />
     </div>
   );
 }
